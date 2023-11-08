38e0e5d16e85f363aa7d5ea309a343b4cc9e4b8c
Simone <simone.carpi@studio.unibo.it>
2023-11-08 17:56:47 +0100

    Aggiunta di metodi alla classe IterableWithPolicyImpl

--- 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
@@ -1,5 +1,78 @@
 package it.unibo.inner.test.impl;
 
-public class IterableWithPolicyImpl {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+import it.unibo.inner.api.IterableWithPolicy;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
+
+    private final List<T> array;
+    private Predicate<T> predicate;
+    //private final T[] array;
+
+    public IterableWithPolicyImpl(final T[] array){
+        //this.array = array;
+        //this.array = List.of(array);
+        this(array, new Predicate<T>() {
+            @Override
+            public boolean test(T arg0) {
+                return true;
+            }
+        });
+    }
+
+    public IterableWithPolicyImpl(final T[] array, final Predicate<T> predicate){
+        this.array = List.of(array);
+        this.predicate = predicate;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+       return new IteratorImpl();
+    }
+
+    @Override
+    public void setIterationPolicy(final Predicate<T> filter) {
+        this.predicate = filter;
+    }
+    
+    public class IteratorImpl implements Iterator<T>{
+
+        private int i = 0;
+        private List<T> list = iteratorArray();
+        @Override
+        public boolean hasNext() {
+            return this.i < this.list.size();
+            /*while(this.i < IterableWithPolicyImpl.this.array.size()){
+                if(IterableWithPolicyImpl.this.predicate.test(array.get(i))){
+                    return true;
+                }
+                i++;
+            }
+            return false;*/
+            //return this.i < IterableWithPolicyImpl.this.array.length;
+        }
+
+        @Override
+        public T next() {
+            return this.list.get(i++);
+            //return IterableWithPolicyImpl.this.array[i++];
+        }
+
+        private List<T> iteratorArray(){
+            List<T> list1 = new ArrayList<>();
+            while(i < IterableWithPolicyImpl.this.array.size()) {
+                if(IterableWithPolicyImpl.this.predicate.test(array.get(i))){
+                    list1.add(array.get(i));
+                }
+                i++;
+            }
+            i = 0;
+            return list1;
+        }
+    }
     
 }
